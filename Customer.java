d249bfca82458907b9bdf0da992782f264034d56
ARULKUMARAN E <arulkumaraneg2006@gmail.com>
2025-10-06 18:17:54 +0530

    Add files via upload

---
+++ Customer.java
@@ -0,0 +1,10 @@
+package PHARMACY; public class Customer { private String id; private String name; private int age;
+public Customer(String id, String name, int age) { this.id = id;
+this.name = name; this.age = age;
+}
+
+
+public String getId() { return id; }
+ 
+public String getName() { return name; }
+}
---
+++ Pharmacist.java
@@ -0,0 +1,8 @@
+package PHARMACY; public class Pharmacist { private String id;
+private String name;
+private String licenseNumber;
+public Pharmacist(String id, String name, String licenseNumber) { this.id = id;
+this.name = name; this.licenseNumber = licenseNumber;
+}
+public String getName() { return name; }
+}
