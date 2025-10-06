6cbc279ce0339b7ca945dba8f55fee337ca47cde
ARULKUMARAN E <arulkumaraneg2006@gmail.com>
2025-10-06 18:29:22 +0530

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,10 +1,16 @@
-package PHARMACY; public class Customer { private String id; private String name; private int age;
-public Customer(String id, String name, int age) { this.id = id;
-this.name = name; this.age = age;
+package PHARMACY; 
+public class Customer { 
+private String id; 
+private String name; 
+private int age;
+public Customer(String id, String name, int age) { 
+this.id = id;
+this.name = name; 
+this.age = age;
 }
-
 
 public String getId() { return id; }
  
 public String getName() { return name; }
 }
+
