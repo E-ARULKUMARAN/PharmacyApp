f3425681fb22ca990dba3fb38c196be9bbe77c90
ARULKUMARAN E <arulkumaraneg2006@gmail.com>
2025-10-06 18:31:20 +0530

    Update PrescriptionItem.java

--- PrescriptionItem.java
+++ PrescriptionItem.java
@@ -1,8 +1,13 @@
 package PHARMACY;
-public class PrescriptionItem { private Medicine medicine; private int quantity;
-public PrescriptionItem(Medicine medicine, int quantity) { this.medicine = medicine;
+public class PrescriptionItem { 
+private Medicine medicine; 
+private int quantity;
+public PrescriptionItem(Medicine medicine, int quantity) { 
+this.medicine = medicine;
 this.quantity = quantity;
 }
  
-public Medicine getMedicine() { return medicine; } public int getQuantity() { return quantity; }
+public Medicine getMedicine() { return medicine; } 
+public int getQuantity() { return quantity; }
 }
+
