8e758b01aadafba7c32ad6b87847cf76bf450431
ARULKUMARAN E <arulkumaraneg2006@gmail.com>
2025-10-06 18:28:42 +0530

    Update Medicine.java

--- Medicine.java
+++ Medicine.java
@@ -1,11 +1,20 @@
 package PHARMACY;
-import java.time.LocalDate; public class Medicine { private String id;
-private String name; private int stock; private double price;
-private LocalDate expiryDate; private boolean prescriptionRequired;
+import java.time.LocalDate; 
+public class Medicine { 
+private String id;
+private String name; 
+private int stock; 
+private double price;
+private LocalDate expiryDate; 
+private boolean prescriptionRequired;
 
 public Medicine(String id, String name, int stock, double price, LocalDate expiryDate, boolean prescriptionRequired) {
-this.id = id; this.name = name; this.stock = stock; this.price = price;
-this.expiryDate = expiryDate; this.prescriptionRequired = prescriptionRequired;
+this.id = id; 
+this.name = name; 
+this.stock = stock; 
+this.price = price;
+this.expiryDate = expiryDate; 
+this.prescriptionRequired = prescriptionRequired;
 }
 public boolean isExpired() {
 return expiryDate.isBefore(LocalDate.now());
@@ -20,7 +29,10 @@ if (quantity <= stock) { stock -= quantity;
 
 
 public String getId() { return id; }
-public String getName() { return name; } public int getStock() { return stock; } public double getPrice() { return price; }
+public String getName() { return name; } 
+public int getStock() { return stock; } 
+public double getPrice() { return price; }
 public LocalDate getExpiryDate() { return expiryDate; }
 public boolean isPrescriptionRequired() { return prescriptionRequired; }
 }
+
